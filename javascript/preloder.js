b99a30d41fb71752caf37ca1da1a35a8f03b2197
rejwan_ahamed <deadpoolzrx@gmail.com>
2022-02-15 22:13:29 +0600

    first commit

---
+++ javascript/preloder.js
@@ -0,0 +1,129 @@
+TweenMax.to(".overlay h1", 2, {
+      opacity: 0,
+      y: -60,
+      ease: Expo.easeInOut
+})
+
+
+
+
+TweenMax.to(".overlay", 2, {
+      delay: 1,
+      top: "-100vh",
+      // top: "-120%",
+      ease: Expo.easeInOut
+})
+
+TweenMax.from(".logo", 1, {
+      delay: 2.4,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+})
+
+
+TweenMax.from(".accordion-item", 1, {
+      delay: 2.4,
+      opacity: 0,
+      y: 20,
+      stagger: 0.1,
+      ease: Expo.easeInOut
+})
+
+
+TweenMax.from(".brandparagrap", 1, {
+      delay: 2.25,
+      opacity: 0,
+      y: 20,
+      stagger: 0.1,
+      ease: Expo.easeInOut
+})
+
+
+TweenMax.staggerFrom(".nav ul li", 1, {
+      delay: 2.4,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+}, 0.2)
+
+TweenMax.staggerFrom(".social-media ul li", 1, {
+      delay: 2.4,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+}, 0.2)
+
+TweenMax.from(".side-strip", 2, {
+      delay: 2.4,
+      opacity: 0,
+      y: 40,
+      ease: Expo.easeInOut
+})
+
+
+
+TweenMax.from(".hero-tital", 1.3, {
+      delay: 2.1,
+      opacity: 0,
+      y: 40,
+      ease: Expo.easeInOut
+})
+
+TweenMax.from(".row", 1.5, {
+      delay: 2.2,
+      opacity: 0,
+      y: 40,
+      ease: Expo.easeInOut
+})
+
+TweenMax.from(".ourdesignsection", 1, {
+      delay: 2.0,
+      opacity: 0,
+      y: 10,
+      ease: Expo.easeInOut
+})
+
+TweenMax.from(".ourdesignsection-paragrap", 1, {
+      delay: 2.1,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+})
+
+
+TweenMax.from("#hfs", 1, {
+      delay: 2.2,
+      opacity: 0,
+      y: 30,
+      ease: Expo.easeInOut
+})
+
+
+TweenMax.from(".row p", 2, {
+      delay: 3.2,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+})
+
+TweenMax.from(".header h1", 2, {
+      delay: 3.2,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+})
+
+TweenMax.from(".header p", 2, {
+      delay: 3.4,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+})
+
+TweenMax.from(".header button", 2, {
+      delay: 3.6,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+})
